cef526ae7bfeb5595a397ecb323cf184f0000263
Ali <alouloucheikh05@gmail.com>
2025-07-28 00:00:45 +0100

    Better Email Formatting

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
     loadProducts().then(() => {
         displayProducts();
         updateCartCount();
-        showWelcomeMessage();
     });
 });
 
